693d5dd0d7ce843a426cfc026ec3f6aafe3c60c8
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-03-13 22:27:30 +0530

    commit

--- script.js
+++ script.js
@@ -2,5 +2,16 @@ let hrs = document.getElementById('hrs')
 let min = document.getElementById('min')
 let sec = document.getElementById('sec')
 
-let currentTime = new Date();
-sec.currentTime
+let stp;
+function start(){
+let s=0;
+stp= setInterval(() => {
+sec.innerHTML= s++;
+}, 1000);
+}
+
+function stop(){
+    clearInterval(stp);
+    let button2 = document.querySelector('.button2')
+    button2.src=""
+}
