01547208fff0b80ea240a1c59ea20c695fc0fb7b
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-03-14 16:18:07 +0530

    change

--- script.js
+++ script.js
@@ -4,23 +4,31 @@ let sec = document.getElementById('sec')
 let button2 = document.querySelector('.button2')
 let button2p = document.querySelector('.button2p')
 button2p.style.display="none"
+
 let stp;
+
 function start(){
 let s=1;
 let m=1;
 let h=1;
 stp= setInterval(() => {
-sec.innerHTML= s++;
+
+sec.innerHTML = s++;
+(s<10?"0":"")
 if(s>=60){
-    min.innerHTML=m++;
-    s=0;
+    min.innerHTML = m++;
+    s=1;
+    if(m>=60){
+        hrs.innerHTML = h++;
+       m=1;
+    }
 }
-}, 1000);
-}
+}, 300);
+} 
 
 
 function stop(){
-    // clearInterval(stp);
+    clearInterval(stp);
 //    let but2p =(button2.src="pausebutton.png")
    but2p.addEventListener('click',()=>{
     but2p.src="playbutton.png"
@@ -31,4 +39,10 @@ function pause(){
 
      button2.style.display="block"
     button2p.style.display="none"
+}
+
+function reset(){
+    sec.innerHTML = "00"
+    min.innerHTML = "00"
+    hrs.innerHTML = "00"
 }
