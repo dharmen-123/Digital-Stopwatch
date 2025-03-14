f83b73d4d29aa466121e9575650a5bcc3f7f22dc
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-03-14 20:40:42 +0530

    commit

--- script.js
+++ script.js
@@ -1,22 +1,28 @@
 let hrs = document.getElementById('hrs')
 let min = document.getElementById('min')
 let sec = document.getElementById('sec')
-let button1 =document.querySelector('.button1')
-let button3 =document.querySelector('.button3')
+
 let button2 = document.querySelector('.button2')
 let button2p = document.querySelector('.button2p')
 
 let p = document.querySelector('p')
-
 p.style.fontSize = "60px"
 p.style.color = "white"
 p.style.fontFamily ="'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif"
 
+let button1 =document.querySelector('.button1')
+let button3 =document.querySelector('.button3')
 button1.setAttribute("style","width:80px; height:80px;")
 button3.setAttribute("style","width:70px; height:70px;")
 button2p.style.display="none"
 
+let screen = document.querySelector('screen')
+screen.setAttribute("style","paddingTop: 10%; wordSpacing: 30px;")
 let stp;
+// .screen{
+//     padding-top: 10%;
+//     word-spacing: 30px;
+// }
 
 function start(){
 let s=1;
