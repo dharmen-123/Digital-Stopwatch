6924aad20ce7bf0496b14a374ac8964c866479de
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-03-14 16:50:55 +0530

    commit

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 let hrs = document.getElementById('hrs')
 let min = document.getElementById('min')
 let sec = document.getElementById('sec')
+let button1 =document.querySelector('.button1')
+let button3 =document.querySelector('.button3')
 let button2 = document.querySelector('.button2')
 let button2p = document.querySelector('.button2p')
+
+let p = document.querySelector('p')
+
+p.style.fontSize = "60px"
+p.style.color = "white"
+p.style.fontFamily ="'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif"
+
+button1.setAttribute("style","width:80px; height:80px;")
+button3.setAttribute("style","width:70px; height:70px;")
 button2p.style.display="none"
 
 let stp;
