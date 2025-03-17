1cadfb1e3febca2ab3a1d539da7848f21beede3f
dharmen-123 <dharmendrachilhate11@gmail.com>
2025-03-17 09:35:28 +0530

    changes

--- script.js
+++ script.js
@@ -12,18 +12,17 @@ p.style.fontFamily = "'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans
 
 let button1 = document.querySelector('.button1')
 let button3 = document.querySelector('.button3')
-button1.setAttribute("style", "width:80px; height:80px;")
+button1.setAttribute("style", "width:70px; height:70px;")
 button3.setAttribute("style", "width:70px; height:70px;")
-// button2p.style.display = "none"
 
 let screen = document.querySelector('screen')
 screen.setAttribute("style", "paddingTop: 10%; wordSpacing: 30px;")
 
-let stp;
+var st
 
 function start(){
     let [s,m,h]=[1,1,1];
- stp = setInterval(()=>{
+ st = setInterval(()=>{
    sec.innerHTML = s++;
    if(s==60){
     s=0;
@@ -38,6 +37,13 @@ function start(){
 }
 
 function stop(){
-    
+    clearInterval(st);
     button2.src ="pausebutton.png"
 } 
+
+function reset(){
+    sec.innerHTML = "00"
+    min.innerHTML = "00"
+    hrs.innerHTML = "00"
+    button2.src="colorplay.png"
+}
